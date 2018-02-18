59835379affa5c654657960d615c2e9f7d6b6edf
fei199225 <fei199225@gmail.com>
2018-02-18 15:02:43 +0800

    update

--- js/app.js
+++ js/app.js
@@ -2,63 +2,6 @@ var counter = 0;
 var map;
 var markers = [];
 
-//硬编码六个地点
-var locations_array = [{
-    title: "北京八达岭长城",
-    desc: "",
-    location: {
-        lat: 40.3597596,
-        lng: 116.0200204
-    },
-    id: "ChIJFbdOF22T8DURnEAe0oca7LM",
-    pageId: "884381"
-}, {
-    title: "北京颐和园",
-    desc: "",
-    location: {
-        lat: 39.9999823,
-        lng: 116.2754606
-    },
-    id: "ChIJP4_Ducf4AjQRMqEETHex2UU",
-    pageId: "252070"
-}, {
-    title: "北京天安门广场",
-    desc: "",
-    location: {
-        lat: 39.9054895,
-        lng: 116.3976317
-    },
-    id: "ChIJ2XRD3Jh2YzYRE1lUrcku6io",
-    pageId: "21193952"
-}, {
-    title: "北京故宫博物院",
-    desc: "",
-    location: {
-        lat: 39.9163447,
-        lng: 116.3971546
-    },
-    id: "ChIJq4HNm91S8DURZGAQm-3qQ94",
-    pageId: "69123"
-}, {
-    title: "北京798艺术区",
-    desc: "",
-    location: {
-        lat: 39.984138,
-        lng: 116.49609
-    },
-    id: "ChIJ8VKWmEqq8TURY4vHHRW3J5M",
-    pageId: "988326"
-},  {
-    title: "北京三里屯",
-    desc: "",
-    location: {
-        lat: 39.935886,
-        lng: 116.455419
-    },
-    id: "ChIJpzA2l5Ss8TURhxCE6XdwnFA",
-    pageId: "998995"
-} ];
-
 function initMap() {
     this.largeInfowindow = new google.maps.InfoWindow();
     // 设置center
@@ -253,8 +196,12 @@ var appViewModel = {
     },
     mapsError: function() {
         alert("Unable to load maps");
+    },
+    //菜单显示隐藏
+    shouldShowMessage:ko.observable(false),
+    show:function () {
+        appViewModel.shouldShowMessage(!appViewModel.shouldShowMessage());
     }
-
 };
 
 appViewModel.query.subscribe(appViewModel.search);
@@ -296,17 +243,7 @@ function getWikiData(location) {
     }
     });
 }
-
-//菜单显示隐藏控制
-function menu_control(){
-    state = document.getElementById("sidebar-wrapper");
-    wrapper = document.getElementById("wrapper");
-    if(state.style.display == "block"){
-        state.style.display = "none";
-        wrapper.style.paddingLeft = "40px";
-    }else{
-        state.style.display = "block";
-        wrapper.style.paddingLeft = "250px";
-    }
-
-}
+//错误处理
+var mapErrorHandler = function(){
+    alert("Details cannot be loaded!");
+};
---
+++ js/locations.js
@@ -0,0 +1,56 @@
+//硬编码六个地点
+var locations_array = [{
+    title: "北京八达岭长城",
+    desc: "",
+    location: {
+        lat: 40.3597596,
+        lng: 116.0200204
+    },
+    id: "ChIJFbdOF22T8DURnEAe0oca7LM",
+    pageId: "884381"
+}, {
+    title: "北京颐和园",
+    desc: "",
+    location: {
+        lat: 39.9999823,
+        lng: 116.2754606
+    },
+    id: "ChIJP4_Ducf4AjQRMqEETHex2UU",
+    pageId: "252070"
+}, {
+    title: "北京天安门广场",
+    desc: "",
+    location: {
+        lat: 39.9054895,
+        lng: 116.3976317
+    },
+    id: "ChIJ2XRD3Jh2YzYRE1lUrcku6io",
+    pageId: "21193952"
+}, {
+    title: "北京故宫博物院",
+    desc: "",
+    location: {
+        lat: 39.9163447,
+        lng: 116.3971546
+    },
+    id: "ChIJq4HNm91S8DURZGAQm-3qQ94",
+    pageId: "69123"
+}, {
+    title: "北京798艺术区",
+    desc: "",
+    location: {
+        lat: 39.984138,
+        lng: 116.49609
+    },
+    id: "ChIJ8VKWmEqq8TURY4vHHRW3J5M",
+    pageId: "988326"
+},  {
+    title: "北京三里屯",
+    desc: "",
+    location: {
+        lat: 39.935886,
+        lng: 116.455419
+    },
+    id: "ChIJpzA2l5Ss8TURhxCE6XdwnFA",
+    pageId: "998995"
+} ];
